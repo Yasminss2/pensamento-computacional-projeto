de3c350721e2764c89056aa3b74b51331789aa2f
Aluna Vocação - Yasmin Santos Sousa <yasminssousousa@gmail.com>
2026-06-25 17:00:54 -0300

    Add files via upload

---
+++ tarde_programacao_2026.2_yasmin/expl_mod04_yasmin.py
@@ -0,0 +1,13 @@
+'''
+listas
+
+''' 
+frutas = ['laranja', 'maçã', 'ameixa', 'abacaxi'] 
+# print (frutas)
+
+print ('Qual fruta que você deseja remover da lista?')
+# nova_fruta = input ()
+nova_fruta  = input ('A escolha foi:')
+frutas.append ('nova fruta')#variavel mais atributo
+#devo usar o ponto"frutas.append(nova-fruta)"
+print(esssas são as novas frutas: {frutas}')
